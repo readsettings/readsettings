b607e28314f46e1c2da7bee695f41dec0dbdc2d9
Richie Bendall <richiebendall@gmail.com>
2018-05-16 17:13:33 +1200

    Changed Code

--- project/readsettings.py
+++ project/readsettings.py
@@ -1,30 +1,53 @@
-from os import remove as rm
+# Setup Code
 
-def create(newsettingspath="appconfig.settings"):
+from os import remove as rm
+from os import rename as rn
+
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+# Functions
+
+def createfile(newsettingspath="appconfig.settings"):
     f = open(newsettingspath,"w+")
 
-def remove(settingspath="appconfig.settings"):
+def removefile(settingspath="appconfig.settings"):
     rm(str(settingspath))
 
-def read(settingspath="appconfig.settings", valuename="setting"):
+def changefilename(settingspath="appconfig.settings", newfilename="appconfig.settings"):
+    rn(str(settingspath), str(newfilename))
+
+def readvalue(settingspath="appconfig.settings", valuename="setting"):
     with open(settingspath, 'r') as myfile:
         lines = myfile.readlines()
     for i in range(len(lines) + 1):
         if lines[i - 1].split(' ')[0] == valuename:
             return str(str(lines[i - 1])[len(str(lines[i - 1].split(' ')[0])) + 1:][0])
 
-def new(settingspath="appconfig.settings", newvaluename="setting", value="Nothing"):
+def newvalue(settingspath="appconfig.settings", newvaluename="setting", value="Nothing"):
     with open(settingspath, 'a') as myfile:
         myfile.write(str(newvaluename) + ' ' + str(value) + '\n')
 
-def change(settingspath="appconfig.settings", valuename="setting", newvalue="Nothing"):
+def changevalue(settingspath="appconfig.settings", valuename="setting", newvalue="Nothing"):
         with open(settingspath, 'r') as myfile:
             lines = myfile.readlines()
         for i in range(len(lines) + 1):
             if lines[i - 1].split(' ')[0] == valuename:
-                lines = open(settingspath, 'r').readlines()
-                lines[lines[i - 1]] = str(valuename) + ' ' + str(newvalue) + '\n'
-                out = open(settingspath, 'w')
-                out.writelines(lines)
-                out.close()
+                replace_line(settingspath, i - 1, str(valuename) + ' ' + str(newvalue) + '\n')
                 break
+
+def changevaluename(settingspath="appconfig.settings", valuename="setting", newname="Nothing"):
+        with open(settingspath, 'r') as myfile:
+            lines = myfile.readlines()
+        for i in range(len(lines) + 1):
+            if lines[i - 1].split(' ')[0] == valuename:
+                replace_line(settingspath, i - 1, str(newname) + ' ' + str((lines[i - 1])[(len(str(lines[i - 1]).split(' ')[0]) + 1):]))
+                break
+
+def rawlines(settingspath="appconfig.settings"):
+    with open(settingspath, 'r') as myfile:
+        return myfile.readlines()
