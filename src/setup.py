8076b482a94572e4c3f9d5656ce59548db5ebf94
Richie Bendall <richiebendall@gmail.com>
2019-03-23 17:10:49 +1300

    Update to 3.0.1

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="readsettings",
-    version="3.0.0",
+    version="3.0.1",
     author="Richie Bendall",
     author_email="richiebendall@gmail.com",
     description=
