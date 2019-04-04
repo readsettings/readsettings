0f8d3fb4a805a43737e2d61beb9519f9ee512756
Richie Bendall <richiebendall@gmail.com>
2019-04-04 11:07:29 +0000

    Make code more efficient

--- src/readsettings/__init__.py
+++ src/readsettings/__init__.py
@@ -6,58 +6,48 @@ import os
 class ReadSettings:
     """Main ReadSettings class."""
 
-    def __init__(self, path):
+    def __init__(self, path="appconfig.json"):
         """Initialise class."""
-        if not path:
-            self.path = "appconfig.json"
-        else:
-            self.path = path
+
+        self.path = path
 
         if not os.path.isfile(path):
             with open(path, "w") as f:
                 f.write("{}")
 
+        with open(self.path, "r") as f:
+            self.data = json.load(f)
+
     def change(self, path, move=False):
         """Change the target directory of the settings file."""
         if move:
             os.rename(self.path, path)
-        else:
-            if not os.path.isfile(path):
-                with open(path, "w") as f:
-                    f.write("{}")
 
-        self.path = path
+        self.__init__(path)
 
     def clear(self):
         """Clear a settings file."""
-        with open(self.path, "w") as f:
-            f.write("{}")
+        with open(self.path, "w") as file:
+            file.write("{}")
 
     def set(self, name, value):
         """Set the value of a setting."""
-        with open(self.path, "r") as f:
-            data = json.load(f)
-            data[name] = value
+        self.data[name] = value
 
-        with open(self.path, "w") as f:
-            json.dump(data, f)
+        with open(self.path, "w") as file:
+            json.dump(self.data, file)
 
     def rem(self, name):
         """Remove a setting."""
-        with open(self.path, "r") as f:
-            data = json.load(f)
-            data.pop(name)
+        self.data.pop(name)
 
-        with open(self.path, "w") as f:
-            json.dump(data, f)
+        with open(self.path, "w") as file:
+            json.dump(self.data, file)
 
     def get(self, name):
         """Get the value of a setting."""
-        with open(self.path, "r") as f:
-            data = json.load(f)
-            return data[name]
+        return self.data[name]
 
     def json(self):
         """Get the raw contents of the settings file."""
-        with open(self.path, "r") as f:
-            return json.load(f)
+        return self.data
