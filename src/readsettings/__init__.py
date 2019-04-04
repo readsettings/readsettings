9c5d20c62fffcaddddf6ca0c2dc16e3a31b24689
Richie Bendall <richiebendall@gmail.com>
2019-04-04 12:20:04 +0000

    Improve code

--- src/readsettings/__init__.py
+++ src/readsettings/__init__.py
@@ -6,47 +6,58 @@ import os
 class ReadSettings:
     """Main ReadSettings class."""
 
-    def __init__(self, path="appconfig.json"):
+    def __init__(self, path):
         """Initialise class."""
-
+        
+        self.autosave = True
         self.path = path
-
+        
         if not os.path.isfile(path):
-            with open(path, "w") as f:
-                f.write("{}")
+            self.data = {}
+        else:
+            with open(self.path, "r") as f:
+                self.data = json.load(f)
 
-        with open(self.path, "r") as f:
-            self.data = json.load(f)
 
-    def change(self, path, move=False):
-        """Change the target directory of the settings file."""
-        if move:
-            os.rename(self.path, path)
-
-        self.__init__(path)
-
+    def autosave(self, option=None):
+        """Configure autosaving"""
+        if option == None:
+            self.autosave = not self.autosave
+        else:
+            self.autosave = option
+        return self.autosave
+        
+    def save(self):
+        """Force a file save"""
+        with open(self.path, "w") as file:
+            json.dump(self.data, file)
+        
+        
     def clear(self):
         """Clear a settings file."""
-        with open(self.path, "w") as file:
-            file.write("{}")
+        self.data = {}
+        
+        if self.autosave:
+            self.save()
 
-    def set(self, name, value):
-        """Set the value of a setting."""
-        self.data[name] = value
-
-        with open(self.path, "w") as file:
-            json.dump(self.data, file)
-
-    def rem(self, name):
-        """Remove a setting."""
-        self.data.pop(name)
-
-        with open(self.path, "w") as file:
-            json.dump(self.data, file)
-
-    def get(self, name):
+    def __getitem__(self, name):
         """Get the value of a setting."""
         return self.data[name]
+            
+    def __setitem__(self, name, value):
+        """Set the value of a setting."""
+        self.data[name] = value
+        
+        if (self.autosave):
+        	self.save()
+        
+
+    def __delitem__(self, name):
+        """Remove a setting."""
+        self.data.pop(name)
+        
+        if (self.autosave):
+        	self.save()
 
     def json(self):
         """Get the raw contents of the settings file."""
