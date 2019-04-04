b6dbce9ceb085f525753e365bf8b456523f9a7fd
Richie Bendall <richiebendall@gmail.com>
2019-04-04 11:09:33 +0000

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="readsettings",
-    version="3.1.0",
+    version="3.1.1",
     author="Richie Bendall",
     author_email="richiebendall@gmail.com",
     description=
