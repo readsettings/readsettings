8f4e4821e221d29c80edc152c187907adef4bf44
Richie Bendall <richiebendall@gmail.com>
2019-04-07 20:38:33 +1200

    Updated documentation resources

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. ReadSettings documentation master file, created by
+   sphinx-quickstart on Sun Apr  7 14:57:29 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ReadSettings's documentation!
+========================================
+
+.. automodule:: readsettings
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -2,27 +2,12 @@
 import setuptools
 
 # Parse the readme markdown file
-with open("../README.md", "r") as fh:
+with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
+# Setup module
 setuptools.setup(
-    name="readsettings",
-    version="3.2.0",
-    author="Richie Bendall",
-    author_email="richiebendall@gmail.com",
-    description=
-    "Easily create, edit and remove a customized settings file which you can use for storing all of the settings for your application.",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/Richienb/readsettings-python",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ),
-)
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'])
